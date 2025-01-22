148c23c596234c422ffb27c6c42651d5e7a481ea
obszank <obszank@gmail.com>
2025-01-22 12:24:02 +0100

    Chapter 10.2.1

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -5,8 +5,28 @@ import java.util.ArrayList;
 
 public class Interpreter implements Expr.Visitor<Object>,
         Stmt.Visitor<Void> {
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+    // private Environment environment = new Environment();
 
-    private Environment environment = new Environment();
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
