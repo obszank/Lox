710fa5dd83136763249813bb446f5745c56b9e9e
obszank <obszank@gmail.com>
2025-01-23 14:46:57 +0100

    Chapter 11.3

---
+++ java/com/craftinginterpreters/lox/Resolver.java
@@ -0,0 +1,14 @@
+package com.craftinginterpreters.lox;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+    private final Interpreter interpreter;
+
+    Resolver(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
+}
