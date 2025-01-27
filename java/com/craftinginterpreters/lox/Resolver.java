1393b1c268b4294296383c5767c1752e293278d1
obszank <obszank@gmail.com>
2025-01-27 10:23:40 +0100

    Chapter 11.5 Resolution Errors

--- java/com/craftinginterpreters/lox/Resolver.java
+++ java/com/craftinginterpreters/lox/Resolver.java
@@ -174,6 +174,9 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             return;
         }
         Map<String, Boolean> scope = scopes.peek();
+        if (scope.containsKey(name.lexeme)) {
+            Lox.error(name, "Already a variable with this name in this scope.");
+        }
         scope.put(name.lexeme, false);
     }
 
